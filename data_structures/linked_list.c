c3b3dc080f90d22d9eda0eef7f7d390b5e6d6f85
FlavioT04 <139927186+FlavioT04@users.noreply.github.com>
2026-06-16 22:36:52 -0230

    iteration throught linked list added

--- data_structures/linked_list.c
+++ data_structures/linked_list.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node
-{
+typedef struct Node{
 	int x;
 	struct Node* next;
 } Node;
 
-int main(void)
-{
+int main(void){
 
 	Node root;
 	
-	root.x = 12;
+	root.x = 12;	// root node
 	root.next = malloc(sizeof(Node));
-	root.next->x = 4;
-	root.next->next = NULL;
-
-	printf("First: %d\n", root.x);	
-	printf("Second: %d\n", root.next->x);
+	root.next->x = 4;	// first node
+	root.next->next = malloc(sizeof(Node));
+	root.next->next->x = 543;	// second node
+	root.next->next->next = NULL;
 	
-	free(root.next); 
+	/* iterate through linked list and print values */
+	Node* curr = &root;
+	while (curr != NULL){
+		printf("%d\n", curr->x);
+		curr = curr->next;
+	}	
+ 
+	free(root.next->next);
+	free(root.next);
 	
 	return 0;
 }	
